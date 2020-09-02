2b336063f24d5a77c18fe02c50027f15cfc626ae
Lukabratzee <lukabratzee@icloud.com>
2020-09-02 17:26:53 +0100

    Update opl.py
    can detect the 5 digits, need to isnert dot after 3

--- opl.py
+++ opl.py
@@ -11,12 +11,15 @@ s_number = re.compile(r'(SLUS-)(\d+)(.*)')
 
 # converts returned result from tuple into str
 def convert_tuple(tup): 
-    values = ','.join(map(str, tup))
+    values = ''.join(map(str, tup))
     return values
 
 # searches for filenames
 for filename in path:
     match = s_number.findall(filename)
+
+    # print (match)
+
     values = convert_tuple(match)
 
     # if one matches, begin formatting
@@ -25,6 +28,7 @@ for filename in path:
         values = values.replace('-', '_')
         # 5 digits to 3 digits, period, then remainder
         # i.e 55555 to 555.55
-        values = re.sub('(?<={5})', '(?<={3})(?>=\.)', values)
+        # can detect the 5 digits, need to isnert dot after 3
+        values = re.sub(r'[0-9]{5,}', r'[0-9]{3,}(?>=\.)', values)
         print(values)
 
