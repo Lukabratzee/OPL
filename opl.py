9e364cb989d969c1ab4e5e459f51a6d6d5e9080e
Lukabratzee <lukabratzee@icloud.com>
2020-09-10 01:17:15 +0100

    Update opl.py
    updated to include vidya filename

--- opl.py
+++ opl.py
@@ -4,44 +4,57 @@ import glob
 
 # example - SLUS_200.74.Summoner
 
-
 # path that points to PS2 game ISO's
 path = glob.iglob('Y:\\Games\\PS2\\' + '**/**', recursive=True)
 
 # compiling regex search. Looks for SLUS-, numbers and ()
 slusRegex = re.compile(r'(SLUS-)(\d+)(.*)')
 numberRegex = re.compile(r'(\d{3})(\d{2})')
+pathRegex = re.compile(r'(?s:.*)(PS2\\)(.*)')
+
+# 'good practice to declare' - Daddy Halloumi
+path_folder = []
+
 # converts returned result from tuple into str
-def convert_tuple(tup): 
+def convert_tuple(tup):
     values = ''.join(map(str, tup))
     return values
 
 # searches for filenames
 for filename in path:
-    match = slusRegex.findall(filename)
 
-    values = convert_tuple(match)
+    match = slusRegex.findall(filename)
+    path_folder.append(filename)
+    tmp = path_folder[-1:]
 
     for x in match:
+        # converts result to string
+        values = convert_tuple(match)
+        tmp = convert_tuple(tmp)
+        # finds last instance of \\ to read parent dir
+        tmp = pathRegex.findall(tmp)
 
         slus, numbers, bracketNumber = x
-
-        numberMatch = numberRegex.findall( numbers )
+        numberMatch = numberRegex.findall(numbers)
         withDot = ".".join(list(numberMatch[0]))
         withDot = withDot + '.'
-        afterDot = ".".join(list(bracketNumber[0:1]))
 
-        # formatting
+        # formatting vidya name
+        up_dir = "".join(list(tmp[0]))
+        up_dir = up_dir.replace('PS2\\', '')
+        up_dir = up_dir.split('\\', 1)[0]
+
+        # formatting ID (don't know how to do this cleaner)
         values = values.replace('-', '_')
         values = values.replace(numbers, withDot)
-        values = values.replace(bracketNumber, afterDot)
+        values = values.replace(bracketNumber, '')
         values = values.replace(' ', '')
         values = values.replace('"', '')
         values = values.replace('\'', '')
         values = values.replace('(', '')
         values = values.replace(')', '')
         values = values.replace(',', '')
-        print (values)
-        os.rename(filename, 'Y:\\Games\\PS2\\' + values + '.iso')
-        
 
+        preview = values + up_dir + '.iso'
+        print (preview)
+        os.rename(filename, 'Y:\\Games\\PS2\\' + preview)
